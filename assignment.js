3a41ce214ef6ea722a32ce994a06cc2490f07efd
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-09-24 20:25:53 +0800

    Git-module-no.1

---
+++ assignment.js
@@ -0,0 +1,18 @@
+//my student information 
+let mname = "Viado"
+let lname = "Bugasto"
+let fullname = (fname + mname + lname)
+let Fullname = fullname.toUpperCase()
+let birthDay = "September 13 2003"
+let birthPlace = "Calaba, Bangued,Abra, philipines, 2800"
+//string concatenation 
+let BirthPlace = birthPlace.toUpperCase()
+let address = "Calaba, Bangued,Abra, philipines, 2800"
+let Address = address.toUpperCase()
+let course = "bachelor of science in computer science"
+let year = "second year"
+let DreamJob = "Game Designer"
+//formating 
+console.log(Fullname + "was born",birthDay + " at", BirthPlace + " and currently living at", Address + ".", fullname + "is taking up", course
+    + "and dreams to be", DreamJob + "after graduation"
+ )
